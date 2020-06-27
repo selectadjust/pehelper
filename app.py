611f3050e9e5c1a83f83efc44cd7f270ff451812
selectadjust <selectadjust@kakao.com>
2020-06-27 22:53:42 +0900

    rev

--- app.py
+++ app.py
@@ -199,7 +199,7 @@ def detailsearching(detail_keyword=None):
 @app.route('/download/<path:file>')
 def download(file):
     guide_n = file
-    guide_a = 'app/templates/guide/'+guide_n
+    guide_a = '/guide/'+guide_n
     return send_file(guide_a)
     
 if __name__ == '__main__':
